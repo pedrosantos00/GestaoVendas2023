d8e8904208b5462affd9b0cfa1ef6dbfb51565d2
Pedro Santos <pedrobocas00@hotmail.com>
2024-04-07 00:58:24 +0100

    Implementação modelo Cliente

---
+++ GestaoVendas2023/pt.com.Gvendas.MODEL/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoVendas2023.pt.com.Gvendas.MODEL
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public int NIF { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Telemovel { get; set; }
+        public string Endereco { get; set; }
+        public string Postal { get; set; }
+        public string Cidade { get; set; }
+        public string Pais { get; set; }
+        public string Profissao { get; set; }
+    }
+}
